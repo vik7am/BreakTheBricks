c9fd34f8dcfaf861b08d26aa25074ea51ee3eda0
Vikrant Kataria <vik7am@gmail.com>
2023-02-28 19:30:15 +0530

    Updated scripts and added numbers on bricks

--- Assets/Script/BallController.cs
+++ Assets/Script/BallController.cs
@@ -4,50 +4,23 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public float speed;
+    [SerializeField] float speed;
     Rigidbody2D rb;
     Vector2 direction;
-    Vector2 velocity;
+
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Start() {
-        velocity = Vector2.zero;
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space))
+            MoveInRandomDirection();
     }
 
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            direction = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
-            velocity = speed * direction.normalized;
-        }
-    }
-
-    private void FixedUpdate() {
-        rb.velocity = velocity;
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.GetComponent<WallController>()){
-            WallController wallController = other.GetComponent<WallController>();
-            int edgeType = wallController.GetEdgeType(transform.position);
-            rb.velocity = direction * speed;
-        }
-    }
-
-    void updateDirection(int edgeType){
-        switch(edgeType){
-            case 0: direction = new Vector2(-direction.x, direction.y); break;
-            case 1: direction = new Vector2(direction.x, -direction.y); break;
-            case 2: direction = new Vector2(-direction.x, -direction.y); break;
-        }
-    }
-
-    public Vector2 GetVelocity(){
-        return rb.velocity;
-    }
-    public void SetVelocity(float x, float y){
-        velocity = new Vector2(x, y);
+    void MoveInRandomDirection(){
+        direction = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
+        //direction = new Vector2(1, 1);
+        direction = direction.normalized;
+        rb.velocity = speed * direction;
     }
 }
--- Assets/Script/BrickController.cs
+++ Assets/Script/BrickController.cs
@@ -1,25 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BrickController : MonoBehaviour
 {
-    BallController ball = null;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] int number;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(ball == null)
-            ball = other.GetComponent<BallController>();
-        Vector2 velocity = ball.GetVelocity();
-        Vector2 diff = ball.transform.position - transform.position;
-        diff = diff.normalized;
-        float absX = Mathf.Abs(diff.x);
-        float absY = Mathf.Abs(diff.y);
-        if( absX > absY)
-            ball.SetVelocity(-velocity.x, velocity.y);
-        else if(absY >absX)
-            ball.SetVelocity(velocity.x, -velocity.y);
-        else
-            ball.SetVelocity(-velocity.x, -velocity.y);
-        Destroy(gameObject);
+    void Start() {
+        updateText();
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        number--;
+        if(number == 0)
+            Destroy(gameObject);
+        updateText();
+    }
+
+    void updateText(){
+        text.text = number.ToString();
     }
 }
